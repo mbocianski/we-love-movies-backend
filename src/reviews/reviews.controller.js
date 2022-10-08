0a121b2c88b43a6437ba789053bc53fff6c28722
mbocianski <miles.a.bocianski@gmail.com>
2022-10-07 18:11:40 -0700

    added heroku install

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -7,20 +7,31 @@ async function reviewIdExists(req,res,next){
     const data = await service.list();
     const foundReview = data.find((review) => reviewId == review.review_id);
     if (foundReview){
-        res.locals.reviewId = foundReview;
+        res.locals.review = foundReview;
         return next();
     }
     next({status: 404, message: "Review cannot be found"})
 }
 
 async function destroy(req,res){
-    const {review_id} = res.locals.reviewId;
-    console.log(review_id);
+    const {review_id} = res.locals.review;
     await service.destroy(review_id);
     res.sendStatus(204);
-
 }
 
+async function update(req,res){
+    const review_id = req.params.reviewId;
+    const updatedReview = {
+        ...req.body.data,
+        review_id: review_id
+    }
+    const review = await service.update(updatedReview);
+    const critic = await service.getCritic(review_id)
+    res.json({data:{...review, ...critic}});
+    };
+
+
 module.exports = {
-    delete: [asyncErrorBoundary(reviewIdExists), asyncErrorBoundary(destroy)]
+    update:[asyncErrorBoundary(reviewIdExists), asyncErrorBoundary(update)],
+    delete:[asyncErrorBoundary(reviewIdExists), asyncErrorBoundary(destroy)]
 }
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,9 +1,15 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties")
 
+const addCritic = mapProperties({
+    preferred_name: "critic.preferred_name",
+    surname: "critic.surname",
+    organization_name: "critic.organization_name"
+})
 
 async function list(){
     return knex("reviews")
-        .select("review_id")
+        .select("*")
 }
 
 async function destroy(review_id){
@@ -12,7 +18,29 @@ async function destroy(review_id){
         .del();
 }
 
+async function update(updatedReview){
+   return knex("reviews")
+        .update(updatedReview, "*")
+        .where({review_id: updatedReview.review_id})
+        .then((updatedReview) => updatedReview[0])
+    
+}
+
+async function getCritic(review_id){
+    return knex("reviews as r")
+    .join("critics as c", "r.critic_id", "c.critic_id")
+    .select("*")
+    .where ({review_id})
+    .first()
+    .then(addCritic)
+}
+
+
+
 module.exports = {
     list,
-    destroy
+    destroy,
+    update,
+    getCritic
 }
+
