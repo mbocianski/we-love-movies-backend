3f2dd479ac33f0cb6c13a009df7c317acba18096
mbocianski <miles.a.bocianski@gmail.com>
2022-10-09 15:49:58 -0700

    added redirect

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ const notFound = require("./errors/notFound")
 // cors for entire app
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => res.status(301).redirect('/movies'));
+
 
 //three routers
 app.use("/movies", moviesRouter);
