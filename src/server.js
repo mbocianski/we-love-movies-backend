51480854d574545001f6f51254c45f1389c41911
Miles <105828519+mbocianski@users.noreply.github.com>
2022-12-05 09:54:20 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ const { PORT = 5000 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-
-// connect to database
 knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
     app.listen(PORT, listener);
   })
-  .catch(console.error);
+  .catch((error) => {
+    console.error(error);
+    knex.destroy();
+  });
+
+function listener() {
+  console.log(`Listening on Port ${PORT}!`);
+}
